d7b5f8ba9a436aff61f88c39790b792dd595a703
malingxoft <malingxoft@gmail.com>
2024-02-25 00:40:50 -0800

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -47,7 +47,7 @@ const userSchema = new Schema(
     position: {
       type: String,
       required: true,
-      enum: ["Driver", "Passenger", "Admin", "User",undefined],
+      enum: ["Driver", "Passenger", "Admin", "User"],
     },
     finished_setting_up: {
       type: Boolean,
@@ -58,7 +58,7 @@ const userSchema = new Schema(
       default: false,
     },
     delete: { type: Boolean, default: false }
-    
+
   },
   { timestamps: true }
 );
